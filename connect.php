b4b8210fe7ba824895744cc10a970333e2297558
assebc <claudioafbessa@gmail.com>
2022-09-04 18:06:50 +0100

    :heavy_plus_sign: .htaccess to have permission to use php

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+    $username = $_POST['user'];
+    $password =  $_POST['pass'];
+
+    //Database connection
+
+    
+
+    //$conn = new mysqli('');
+
+    // Validate Log In
+    if(isset($_POST['but_login']) echo 'User found!';
+?>
