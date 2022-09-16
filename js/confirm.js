c7491ab2547e3880c177e94c4a33cf15f0da4f58
assebc <claudioafbessa@gmail.com>
2022-09-16 14:33:01 +0100

    :bug: custom validity not appearing

--- js/confirm.js
+++ js/confirm.js
@@ -1,9 +1,11 @@
 function onChange() {
     const password = document.querySelector('input[name=password]');
     const confirm = document.querySelector('input[name=confirm]');
-    if (confirm.value === password.value) {
-      confirm.setCustomValidity('');
-    } else {
-      confirm.setCustomValidity('Passwords do not match');
+    const but = document.querySelector('button[name=but_login]');
+    if (confirm.value != password.value) {
+        confirm.setCustomValidity('Passwords do not match');
+    } else{
+        but.href="index.html"
     }
+    confirm.reportValidity();
   }
