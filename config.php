ab659af8f8cf7c3dc22e10c4aa76c3dd075ce79c
bessa_ <claudioafbessa@gmail.com>
2022-10-11 16:58:47 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
     $dbHost = 'Localhost';
     $dbUsername = 'root'
-    $dbPassword = 'Boybessa1';
+    $dbPassword = '';
     $dbName = 'login-template'
 
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
