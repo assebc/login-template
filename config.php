082d75f9731e9dc1162348c01132efcfcf25df65
assebc <claudioafbessa@gmail.com>
2022-10-11 14:56:06 +0100

    :heavy_plus_sign: DB configuration

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root'
+    $dbPassword = 'Boybessa1';
+    $dbName = 'login-template'
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    //if($conexao->connect_errno){
+    //    echo "Error connecting"
+    //} else{
+    //    echo "Connected sucessfully"
+    //}
+
+?>
