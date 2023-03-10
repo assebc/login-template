6a7c7b9a12e4ba3ade6083d57603f4577ae66004
assebc <claudioafbessa@gmail.com>
2023-03-10 12:15:21 +0000

    feat: thank you page

--- frontend/src/pages/ThankYou/index.tsx
+++ frontend/src/pages/ThankYou/index.tsx
@@ -1,9 +1,15 @@
 import { FC } from "react";
+import Skate from "../../assets/skate_buddies.svg";
+import "../../styles.css";
 
 export const ThankYou: FC = () => {
 
   return (
-    <>
-    </>
+    <div className="main_login">
+        <div className="left_login">
+            <h1>Welcome in<br /> this is our world!</h1>
+            <img src={Skate} alt="presentation-image" />
+        </div>
+    </div>
   );
 };
