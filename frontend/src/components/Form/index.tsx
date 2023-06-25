c9730cc2f07edb6f977f27a64ca8c0c118618403
assebc <claudioafbessa@gmail.com>
2023-06-25 08:20:22 +0100

    refactor: refactoring form using antdesign components

--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -1,7 +1,9 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Form as AntdForm, Input } from "antd";
 import "./styles.css";
 
+type LayoutType = 'vertical';
 interface IFormProps {
   children: ReactNode;
   login?: boolean;
@@ -9,7 +11,6 @@ interface IFormProps {
   password?: boolean;
   onClick: () => void;
 }
-
 export const Form: FC<IFormProps> = ({
   children,
   login,
@@ -17,88 +18,87 @@ export const Form: FC<IFormProps> = ({
   password,
   onClick,
 }) => {
-
+  const [form] = AntdForm.useForm();
+  const [formLayout, setFormLayout] = useState<LayoutType>('vertical');
   const navigate = useNavigate();
 
+  const onFormLayoutChange = ({ layout }: { layout: LayoutType }) => {
+    setFormLayout(layout);
+  };
+
   return (
-    <div className="form">
+    <AntdForm
+      layout={formLayout}
+      form={form}
+      initialValues={{layout: formLayout}}
+      className="form"
+    >
       <div className="card_form">
+
         <h1>{children}</h1>
 
         <div className="textfield">
           <label htmlFor="user">Username</label>
-          <input 
-            type="text" 
-            name="username" 
-            placeholder="Insert username" 
-            required 
-            autoComplete="off"
-          />
+          <input type="text" name="username" placeholder="Insert username" required autoComplete="off" />
         </div>
 
-        <div className="textfield">
-          { register || login ? (
-            <label htmlFor="pass">Password</label> 
-          ) : (
-            <label htmlFor="pass">New Password</label>
-          )}
-          
-          <input 
-            type="password" 
-            name="password" 
-            placeholder="Insert password" 
-            required 
-            autoComplete="off"
-          />
-
-        </div>
+        { register || login ? (
+          <div className="textfield">
+            <label htmlFor="password">Password</label>
+            <Input type="password" name="password" placeholder="Insert password" required autoComplete="off" />
+          </div>
+        ) : (
+          <div className="textfield"> 
+            <label htmlFor="password">New Password</label>
+            <Input type="password" name="password" placeholder="Insert password" required autoComplete="off" />
+          </div>
+        )}
         
         { register || password ? (
           <div className="textfield">
-            <label htmlFor="pass">Confirm Password</label>
-            <input 
-              type="password" 
-              name="confirm_password" 
-              placeholder="Confirm password" 
-              required 
-              autoComplete="off"
-            />
-          </div>
-
-        ): undefined }
-
-        <div className="no_account">
-          { login ? (
-            <a 
-              onClick={() => navigate("/forgot-password")}>
-                Forgot password? Click here!
-            </a> 
-          ) : undefined }
-
-          { register ? (
-            <a 
-              onClick={() => navigate("/")}>
-                Already have and account? Please login here!
-              </a>
-          ) : undefined }
-        </div>
-          
-        <button 
-          className="button_login" 
-          name="button_login"
-          onClick={onClick}>
-          {children}
-        </button>
-        
-        { login || password ? (
-          <div className="no_account" style={{marginTop: "12px"}}>
-            <a onClick={() => navigate("/register")}>
-              Do not have an account? Register here!
-            </a>
+            <label htmlFor="password">Confirm Password</label>
+            <Input type="password" name="confirm_password" placeholder="Confirm password" required autoComplete="off" />
           </div>
         ) : undefined }
 
+        <AntdForm.Item className="no_account">
+          { login ? (
+            <a
+              onClick={() => navigate("/forgot-password")}
+            >
+              Forgot password? Click here!
+            </a>
+          ) : undefined}
+        </AntdForm.Item>
+
+        <button
+          className="button_login"
+          name="button_login"
+          onClick={onClick}
+        >
+          {children}
+        </button>
+
+        { login || password ? (
+          <AntdForm.Item className="no_account" style={{marginTop: "12px"}}>
+            <a onClick={() => navigate("/register")}>
+              Do not have an account? Register here!
+            </a>
+          </AntdForm.Item>
+        ) : undefined }
+
+        { register ? (
+          <AntdForm.Item className="no_account" style={{marginTop: "12px"}}>
+          <a 
+            onClick={() => navigate("/")}
+          >
+            Already have and account? Please login here!
+          </a>
+          </AntdForm.Item>
+        ) : undefined }
+        
       </div>
-    </div>
+    </AntdForm>
+
   );
 };
